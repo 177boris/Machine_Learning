f6fe010139200d56ffb2d0c71c67990a2caf6d7d
177boris <lanreboris11@yahoo.com>
2022-06-18 20:02:32 +0100

    recent ML practice

---
+++ NeuralNetworks/visualise.py
@@ -0,0 +1,25 @@
+import keras
+from matplotlib.pyplot import title
+from keras.models import Sequential,Input,Model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.advanced_activations import LeakyReLU
+import tensorflow as tf
+from tensorflow import keras
+
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=(28,28,1),padding='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D((2, 2),padding='same'))
+model.add(Conv2D(64, (3, 3), activation='linear',padding='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
+model.add(LeakyReLU(alpha=0.1))                  
+model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+model.add(Flatten())
+model.add(Dense(128, activation='linear'))
+model.add(LeakyReLU(alpha=0.1))                  
+model.add(Dense(500, activation='softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
